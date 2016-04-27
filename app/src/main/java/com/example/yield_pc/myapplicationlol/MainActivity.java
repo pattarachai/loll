6050265d97392b6d38c2f3fb94c4e757fa7a1046
Yield-PC <pattarachai@ccn-tech.co.th>
2016-04-27 08:47:14 +0700

    UPDATE

--- app/src/main/java/com/example/yield_pc/myapplicationlol/MainActivity.java
+++ app/src/main/java/com/example/yield_pc/myapplicationlol/MainActivity.java
@@ -1,13 +1,73 @@
 package com.example.yield_pc.myapplicationlol;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
-
+    CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FacebookSdk.sdkInitialize(getApplicationContext());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("com.example.yield_pc.myapplicationlol",         PackageManager.GET_SIGNATURES);//Project Name
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String sign=Base64.encodeToString(md.digest(), Base64.DEFAULT);
+                Log.d("KEY HASH:", sign);
+                Toast.makeText(getApplicationContext(),sign,         Toast.LENGTH_LONG).show();
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+        } catch (NoSuchAlgorithmException e) {
+        }
+
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+                Intent i = new Intent(MainActivity.this , Main2Activity.class);
+                startActivity(i);
+
+
+            }
+
+            @Override
+            public void onCancel() {
+
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+
+            }
+        });
+
+
+        //lolololo
     }
 }
